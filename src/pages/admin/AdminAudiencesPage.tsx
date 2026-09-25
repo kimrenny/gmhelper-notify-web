2a57538c72ec216cbd05ab5591ecd1e1e66dfa66
kimrenny <sevdon4@gmail.com>
2026-09-25 21:31:34 +0300

    refactor(admin): remove obsolete page scaffolding

--- src/pages/admin/AdminAudiencesPage.tsx
+++
@@ -1,5 +0,0 @@
-function AdminAudiencesPage() {
-  return <section style={{ padding: '2rem' }}>Audiences placeholder</section>
-}
-
-export default AdminAudiencesPage
--- src/pages/admin/AdminNotificationsPage.tsx
+++
@@ -1,5 +0,0 @@
-function AdminNotificationsPage() {
-  return <section style={{ padding: '2rem' }}>Notifications dashboard placeholder</section>
-}
-
-export default AdminNotificationsPage
